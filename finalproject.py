142cdd7a4196d10c8551402d9a6ecb4ded0a793c
jennachoo <seohyunchoo@gmail.com>
2017-12-01 13:12:46 -0500

    added textblob that does sentiment analysis

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,6 @@
+import re
 import pandas as pd
+from textblob import TextBlob
 
 
 # read & parse data
@@ -10,9 +12,46 @@ import pandas as pd
 testfile = "Twitter_Data_Test.xlsx"
 df = pd.read_excel(testfile)
 
-texts = df["Sound Bite Text"]
+tweets = df["Sound Bite Text"]
 
 
-# do the magical algorithm
+
+# sentiment analysis
+# http://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
+def clean_tweet(tweet):
+    '''
+    Utility function to clean tweet text by removing links, special characters
+    using simple regex statements.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def get_tweet_sentiment(tweet):
+    '''
+    Utility function to classify sentiment of passed tweet
+    using textblob's sentiment method
+    '''
+    # create TextBlob object of passed tweet text
+    analysis = TextBlob(clean_tweet(tweet))
+    # set sentiment
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+
+
+negative = 0
+positive = 0
+neutral = 0
+for tweet in tweets:
+	sent = get_tweet_sentiment(clean_tweet(tweet))
+	if sent == 'positive':
+		positive += 1
+	elif sent == 'negative':
+		negative += 1
+	else:
+		neutral += 1
+
 
 # make it look nice
